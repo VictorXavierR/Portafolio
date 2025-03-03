a1aa08d3b542f6794d1812431a9c5933e16af191
Victor Xavier Rodriguez <victorxr1994@gmail.com>
2025-03-03 14:07:07 +0100

    Solucionado error en el recaptcha

--- src/services/emailService.tsx
+++ src/services/emailService.tsx
@@ -1,16 +1,14 @@
 import emailjs from 'emailjs-com';
 
 export const sendEmail = (form: HTMLFormElement, captchaValue: string) => {
-    // Agregar el valor del reCAPTCHA a los datos del formulario
-    const formData = new FormData(form);
-    formData.append('g-recaptcha-response', captchaValue);  // Agregamos el valor del reCAPTCHA
-  
-    // Enviar el formulario a EmailJS
-    form.append('g-recaptcha-response', captchaValue);  // Agregamos el valor del reCAPTCHA
-    return emailjs.sendForm('service_oexwfbp', 'template_bep8nmo', form, 'ajY1xopH6osuCmuP1')
-      .then((result) => {
-        console.log('Correo enviado con éxito:', result.text);
-      }, (error) => {
-        console.log('Error al enviar el correo:', error.text);
-      });
-  };
+  // Crear un nuevo FormData con el valor del reCAPTCHA
+  const formData = new FormData(form);
+  formData.append('g-recaptcha-response', captchaValue);
+  // Enviar el formulario a EmailJS
+  return emailjs.sendForm('service_oexwfbp', 'template_bep8nmo', form, 'ajY1xopH6osuCmuP1')
+    .then((result) => {
+      console.log('Correo enviado con éxito:', result.text);
+    }, (error) => {
+      console.log('Error al enviar el correo:', error.text);
+    });
+};
